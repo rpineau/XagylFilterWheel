a3ec97666f31aab4b1dc6904b4a5c418449e9dc7
Rodolphe Pineau <pineau@rti-zone.org>
2017-01-14 12:09:53 -0800

    Adding RPI installer

--- installer/rpi_install.sh
+++ installer/rpi_install.sh
@@ -18,15 +18,15 @@ if [ ! -d "$TheSkyX_Path" ]; then
 fi
 
 cp "./filterwheellist xagyl.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./XagylFilterWheel.ui" "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/"
-cp "./Xagyl.png" "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/"
-cp "./libXagylFilterWheel.so" "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/"
+cp "./XagylFilterWheel.ui" "$TheSkyX_Path/Resources/Common/PlugInsARM32/FilterWheelPlugIns/"
+cp "./Xagyl.png" "$TheSkyX_Path/Resources/Common/PlugInsARM32/FilterWheelPlugIns/"
+cp "./libXagylFilterWheel.so" "$TheSkyX_Path/Resources/Common/PlugInsARM32/FilterWheelPlugIns/"
 
 app_owner=`/usr/bin/stat -c "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
 	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/filterwheellist xagyl.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/XagylFilterWheel.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/Xagyl.png"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/libXagylFilterWheel.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugInsARM32/FilterWheelPlugIns/XagylFilterWheel.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugInsARM32/FilterWheelPlugIns/Xagyl.png"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugInsARM32/FilterWheelPlugIns/libXagylFilterWheel.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/libXagylFilterWheel.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/PlugInsARM32/FilterWheelPlugIns/libXagylFilterWheel.so"
