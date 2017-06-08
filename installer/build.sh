2fbdb10cf0b63d554c6c5ecf8b8e40f4d91be50a
Rodolphe Pineau <pineau@rti-zone.org>
2017-06-07 18:52:47 -0700

    Fix directory name

--- installer/build.sh
+++ installer/build.sh
@@ -8,10 +8,10 @@ cp "../build/Release/libXagylFilterWheel.dylib" ROOT/tmp/XagylFilterWheel_X2/
 
 if [ ! -z "$installer_signature" ]; then
 # signed package using env variable installer_signature
-pkgbuild --root ROOT --identifier org.rti-zone.XagylFilterWheel_X2 --sign "$installer_signature" --scripts Scritps --version 1.0 XagylFilterWheel_X2.pkg
+pkgbuild --root ROOT --identifier org.rti-zone.XagylFilterWheel_X2 --sign "$installer_signature" --scripts Scripts --version 1.0 XagylFilterWheel_X2.pkg
 pkgutil --check-signature ./XagylFilterWheel_X2.pkg
 else
-pkgbuild --root ROOT --identifier org.rti-zone.XagylFilterWheel_X2 --scripts Scritps --version 1.0 XagylFilterWheel_X2.pkg
+pkgbuild --root ROOT --identifier org.rti-zone.XagylFilterWheel_X2 --scripts Scripts --version 1.0 XagylFilterWheel_X2.pkg
 fi
 
 rm -rf ROOT
