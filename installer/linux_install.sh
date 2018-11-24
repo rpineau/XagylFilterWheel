8b0b3c120697468dc6f3be79540422ecbbd0e382
Rodolphe Pineau <pineau@rti-zone.org>
2018-11-23 18:31:32 -0800

    fix linux install script as apparently the new plugin directory will be PlugIns64

--- installer/linux_install.sh
+++ installer/linux_install.sh
@@ -6,9 +6,7 @@ echo "TheSkyX_Install = $TheSkyX_Install"
 if [ ! -f "$TheSkyX_Install" ]; then
     echo TheSkyXInstallPath.txt not found
     TheSkyX_Path=`/usr/bin/find ~/ -maxdepth 3 -name TheSkyX`
-    if [ -d "$TheSkyX_Path" ]; then
-		TheSkyX_Path="${TheSkyX_Path}/Contents"
-    else
+    if [ ! -d "$TheSkyX_Path" ]; then
 	   echo TheSkyX application was not found.
     	exit 1
 	 fi
@@ -24,16 +22,22 @@ if [ ! -d "$TheSkyX_Path" ]; then
     exit 1
 fi
 
+if [ -d "$TheSkyX_Path/Resources/Common/PlugIns64" ]; then
+	PLUGINS_DIR="PlugIns64"
+else
+	PLUGINS_DIR="PlugIns"
+fi
+
 cp "./filterwheellist xagyl.txt" "$TheSkyX_Path/Resources/Common/Miscellaneous Files/"
-cp "./XagylFilterWheel.ui" "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/"
-cp "./Xagyl.png" "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/"
-cp "./libXagylFilterWheel.so" "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/"
+cp "./XagylFilterWheel.ui" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FilterWheelPlugIns/"
+cp "./Xagyl.png" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FilterWheelPlugIns/"
+cp "./libXagylFilterWheel.so" "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FilterWheelPlugIns/"
 
 app_owner=`/usr/bin/stat -c "%u" "$TheSkyX_Path" | xargs id -n -u`
 if [ ! -z "$app_owner" ]; then
 	chown $app_owner "$TheSkyX_Path/Resources/Common/Miscellaneous Files/filterwheellist xagyl.txt"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/XagylFilterWheel.ui"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/Xagyl.png"
-	chown $app_owner "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/libXagylFilterWheel.so"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FilterWheelPlugIns/XagylFilterWheel.ui"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FilterWheelPlugIns/Xagyl.png"
+	chown $app_owner "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FilterWheelPlugIns/libXagylFilterWheel.so"
 fi
-chmod  755 "$TheSkyX_Path/Resources/Common/PlugIns/FilterWheelPlugIns/libXagylFilterWheel.so"
+chmod  755 "$TheSkyX_Path/Resources/Common/$PLUGINS_DIR/FilterWheelPlugIns/libXagylFilterWheel.so"
